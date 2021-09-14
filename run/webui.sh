21ce975e67078064471ea4ecdd664e77af92bc4c
August.8 <chchcome@163.com>
2021-09-14 09:54:06 +0800

    Update webui.sh

--- run/webui.sh
+++ run/webui.sh
@@ -10,7 +10,7 @@ if [[ "${ENABLE_WEBUI_CONSOLE}" == "yes" ]]; then
 
 	echo "[info] Starting Minecraft console Web UI..."
 	# note - do NOT quote the credentials, it will not start otherwise
-	/usr/local/bin/gotty --port=8222 --title-format "${WEBUI_CONSOLE_TITLE}" ${credentials} --permit-write screen -x minecraft
+	sh /usr/local/bin/gotty --port=8222 --title-format "${WEBUI_CONSOLE_TITLE}" ${credentials} --permit-write screen -x minecraft
 
 else
 
